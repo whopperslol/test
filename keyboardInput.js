06d3eeb071f41bb758af98f10e973a0e39d1918f
whopperslol <whopperslol7872@gmail.com>
2026-05-11 22:02:54 -0700

    shows last key pressed

--- keyboardInput.js
+++ keyboardInput.js
@@ -1,7 +1,10 @@
-console.log("keyboardInput.js start init...");
+console.log("keyboardInput.js start init...")
+
+const keyDisplay = document.getElementById("lastKey")
 
 document.addEventListener("keypress", function(keyPressed){ 
   console.log(keyPressed);
+  keyDisplay.textContent = keyPressed;
 });
 
 console.log("keyboardInput.js initialized!");
